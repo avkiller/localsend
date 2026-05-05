7bc9122ae9323fb5b9b63a58af1a531816fe798b
fireworld <fuxudong103@gmail.com>
2026-05-05 10:49:01 +0800

    修改apk输出详情

--- common/lib/constants.dart
+++ common/lib/constants.dart
@@ -19,7 +19,7 @@ const fallbackProtocolVersion = '1.0';
 
 /// The default http server port and
 /// and multicast port.
-const defaultPort = 53317;
+const defaultPort = 58888
 
 /// The default discovery timeout in milliseconds.
 /// This is the time the discovery server waits for responses.
