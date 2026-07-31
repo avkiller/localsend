a6979036227cd62bc29f04b1d85863caebccac01
avkiller <avkiller2012@gmail.com>
2026-07-31 17:28:04 +0800

    Update persistence_provider.dart

--- app/lib/provider/persistence_provider.dart
+++ app/lib/provider/persistence_provider.dart
@@ -162,7 +162,7 @@ class PersistenceService {
     }
 
     if (prefs.getString(_aliasKey) == null) {
-      await prefs.setString(_aliasKey, generateDeviceAlias(ref));
+      await prefs.setString(_aliasKey, generateDeviceAlias());
     }
 
     if (prefs.getString(_securityContext) == null) {
